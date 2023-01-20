d7cf957516ba8a69778b3f02cab92c348e757ed9
vikas jat <vjchoudhary1234@gmail.com>
2023-01-20 13:45:30 +0530

    solution of pizza billing

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,17 +2,17 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
+    DeluxePizza dp = new DeluxePizza(false);
+    System.out.println("Base Price Of The Pizza: "+dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
-    System.out.println(dp.getBill());
+    System.out.println("Total price:" +" "+ dp.getBill());
 
     Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
-    System.out.println(p.getBill());
+    System.out.println("Total price:"+" "+p.getBill());
   }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,84 @@
 package com.driver;
 
+import com.sun.source.tree.ReturnTree;
+
 public class Pizza {
 
     private int price;
     private Boolean isVeg;
     private String bill;
+    private boolean took =false;
+  //  public  int price1 =0;
+    public boolean added = false;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(this.isVeg){
+            this.price=300;
+        }
+        else{
+            this.price=400;
+        }
+
     }
 
     public int getPrice(){
-        return this.price;
+
+       // return this.price;
+        if(isVeg==true){
+
+            this.price=300;
+            return this.price;
+        }
+        else{
+
+            this.price=400;
+            return this.price;
+        }
+
     }
 
     public void addExtraCheese(){
         // your code goes here
+
+        if(added==false){
+            System.out.println("Extra Cheese Added: "+80);
+          this.price+=80;
+            added=true;
+        }
+        else{
+          this.price=this.price;
+        }
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg){
+            System.out.println("Extra Toppings Added: "+70);
+
+            this.price+=70;
+        }
+        else{
+            System.out.println("Extra Toppings Added: " +120);
+
+            this.price+=120;
+        }
     }
 
-    public void addTakeaway(){
+    public void addTakeaway() {
         // your code goes here
+        if (took == false) {
+            System.out.println("Paperbag Added: "+20);
+            this.price += 20;
+            took = true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+this.bill = Integer.toString(this.price);
+        return bill;
     }
 }
