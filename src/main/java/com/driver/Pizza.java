1aa58cc6ce8c2ea455b40ba7c88297d74406842f
vikas1509 <vjchoudhary1234@gmail.com>
2023-02-01 19:42:22 +0530

    solving

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,110 +1,81 @@
 package com.driver;
 
-import com.sun.source.tree.ReturnTree;
-
 public class Pizza {
 
     private int price;
-    private int cheesprice;
-    private int takeAway;
-    private int topingPrice;
     private Boolean isVeg;
     private String bill;
-     private boolean exctraChees;
-
-    private boolean addTakeAway ;
-    private boolean addexctraToping ;
-    private boolean addExctraChees;
+    private int cheesePrice;
+    private int toppingsPrice;
+    private int takeAway;
+    private boolean addExtraCheese;
+    private boolean addExtraToppings;
+    private boolean addTakeAway;
     private boolean isBillGeneration;
-  //  public  int price1 =0;
-    public boolean added = false;
 
     public Pizza(Boolean isVeg){
+        this.isVeg = isVeg;
+        this.cheesePrice = 80;
+        this.takeAway = 20;
 
-        // your code goes here
-        this.isVeg=isVeg;
-        this.cheesprice=80;
-        this.takeAway=20;
-        if(this.isVeg=true){
-            this.price=300;
-            this.topingPrice=20;
-        }
         if(isVeg==true){
-            this.price=300;
-            this.topingPrice=70;
+            this.price = 300;
+            this.toppingsPrice = 70;
         }
         else{
-            this.price=400;
-            this.topingPrice=120;
+            this.price = 400;
+            this.toppingsPrice = 120;
         }
-        this.addExctraChees=false;
-        this.addTakeAway=false;
-        this.addexctraToping=false;
-        this.isBillGeneration=false;
-        this.bill="Base price of the pizza: "+ this.price+"\n";
-
-
+        this.addExtraCheese = false;
+        this.addTakeAway = false;
+        this.addExtraToppings = false;
+        this.isBillGeneration = false;
+        this.bill = "Base Price Of The Pizza: " + this.price+"\n";
     }
 
     public int getPrice(){
-
-       // return this.price;
-     return this.price;
-
+        return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
-
-        if(addExctraChees==false){
-           this.price+=this.cheesprice;
-
-      addExctraChees=true;
-
+        if(addExtraCheese==false){
+            this.price += this.cheesePrice;
+            addExtraCheese = true;
         }
-
-
     }
 
     public void addExtraToppings(){
         // your code goes here
-        if(addexctraToping==false){
-           this.price+=this.topingPrice;
-           addexctraToping=true;
-
-
-
+        if(addExtraToppings == false) {
+            this.price = this.price + this.toppingsPrice;
+            addExtraToppings = true;
         }
-
     }
 
-    public void addTakeaway() {
+    public void addTakeaway(){
         // your code goes here
-        if (addTakeAway == false) {
-
-            this.price +=this.takeAway;
-            addTakeAway =true;
-
+        if(addTakeAway == false){
+            this.price = this.price + this.takeAway;
+            addTakeAway = true;
         }
     }
 
     public String getBill(){
         // your code goes here
-if(isBillGeneration==false){
-    if(addExctraChees==true){
-        this.bill+="Exctra Chees Added: "+this.cheesprice+"\n";
-
-    }
-    if(addexctraToping==true){
-        this.bill+="Exctra Toping Added: "+this.topingPrice+ "\n";
-    }
-    if(addTakeAway==true) {
-        this.bill += "Paperbag Added: " + this.takeAway + "\n";
-    }
-    this.bill+="Total price: "+this.price+"\n";
-    isBillGeneration=true;
-
-}
-return this.bill;
+        if(isBillGeneration == false){
+            if(addExtraCheese==true){
+                this.bill += "Extra Cheese Added: "+this.cheesePrice+"\n";
+            }
+            if(addExtraToppings == true) {
+                this.bill += "Extra Toppings Added: " + this.toppingsPrice + "\n";
+            }
+            if(addTakeAway == true){
+                this.bill += "Paperbag Added: "+this.takeAway+"\n";
+            }
+            this.bill+="Total Price: "+this.price+"\n";
+            isBillGeneration = true;
+        }
+        return this.bill;
     }
 }
